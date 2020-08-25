8e86c7c85272f7006a5b2cc83ede89708b90a315
kenta-crv <adp@crold-valieat.jp>
2020-08-25 23:57:17 +0900

    first

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "介護施設・老人ホームを知るなら｜介護の里",
+      site: "地元で最良のジムを見つけるなら｜ジモジム",
       title: "<%= yield(:title) || eatery-japan %>" ,
-      description: "介護施設・老人ホームを知るなら介護の里",
-      keywords:    "介護施設,老人ホーム",
+      description: "地元で最良のジムを見つけるならジモジム",
+      keywords:    "ジム,地元",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       #icon: [href: image_url('favicon.ico')]
